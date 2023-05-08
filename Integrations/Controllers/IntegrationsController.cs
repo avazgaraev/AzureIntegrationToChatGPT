ded13bc5bc3d540962aba285dc2338b46c124c14
Avaz <avazgarayev@gmail.com>
2023-05-08 10:14:54 +0400

    First

---
+++ Integrations/Controllers/IntegrationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+
+namespace Integrations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegrationsController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public async Task<string> Sender(string request)
+        {
+            OpenAIAPI api = new OpenAIAPI("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");
+            var result = await api.Chat.CreateChatCompletionAsync(new ChatRequest()
+            {
+                Model = Model.ChatGPTTurbo,
+                Temperature = 0.1,
+                MaxTokens = 1024,
+                Messages = new ChatMessage[] {
+                    new ChatMessage(ChatMessageRole.User, request)
+                }
+            });
+
+            var reply = result.Choices[0].Message;
+
+            return reply.Content;
+        }
+    }
+}
---
+++ Integrations/SpeechClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CognitiveServices.Speech;
+
+namespace Integrations
+{
+    public class SpeechClass
+    {
+        static string speechKey = Environment.GetEnvironmentVariable("f9778c8be7254bdb97d461c84cfefffa");
+        static string speechRegion = Environment.GetEnvironmentVariable("eastus");
+
+        static void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
+        {
+            switch (speechSynthesisResult.Reason)
+            {
+                case ResultReason.SynthesizingAudioCompleted:
+                    Console.WriteLine($"Speech synthesized for text: [{text}]");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
